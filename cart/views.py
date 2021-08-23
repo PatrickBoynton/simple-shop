8ccfb3c20540db47c16de0d29a8fb94f8acd8473
PatrickBoynton <jpboynton3@gmail.com>
2021-08-23 18:25:53 -0400

    Added basic views.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Cart
+from .serializers import CartSerializer
+
 
 # Create your views here.
+class CartListView(generics.ListAPIView):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
