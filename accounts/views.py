f5a2cd98df57b48ac62b550371ae8446554811d9
PatrickBoynton <jpboynton3@gmail.com>
2021-08-23 18:26:39 -0400

    Created the initial profile views.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Profile
+from .serializers import ProfileSerializer
+
 
 # Create your views here.
+class ProfileListView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class ProfileCreateView(generics.CreateAPIView):
+    # Todo add permissions.
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class ProfileRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Profile.objects.filter(user=user)
