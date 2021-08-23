c40aa7fed43f71bdde7cc8d11c3bfa6291cc6399
PatrickBoynton <jpboynton3@gmail.com>
2021-08-23 18:24:49 -0400

    Created basic serializers just to get it going.

---
+++ wishlist/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import WishList
+
+
+class WishListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WishList
+        fields = '__all__'
