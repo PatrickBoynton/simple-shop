1e868c9a6fe9a6b2130ac4cf39ebbbdfcd96e59c
PatrickBoynton <jpboynton3@gmail.com>
2021-08-23 18:28:30 -0400

    Uncommented out the accounts url.

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 app_name = 'api_v1'
 
 urlpatterns = [
-    # path('accounts/', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('cart/', include('cart.urls')),
     path('product/', include('product.urls')),
     path('wishlist/', include('wishlist.urls')),
