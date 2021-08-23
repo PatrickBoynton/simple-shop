8979dc611a472439e42f9a128f93f98adda741f3
PatrickBoynton <jpboynton3@gmail.com>
2021-08-23 18:27:15 -0400

    Created an initial wishlist view.

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import WishList
+from .serializers import WishListSerializer
+
 
 # Create your views here.
+class WishListListView(generics.ListAPIView):
+    queryset = WishList.objects.all()
+    serializer_class = WishListSerializer
