ee95b79bc54bb2aa061b6d5b76b4a9955d401f8f
PatrickBoynton <jpboynton3@gmail.com>
2021-09-01 14:22:38 -0400

    Added a temporary display of products.

--- frontend/static/src/App.tsx
+++ frontend/static/src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC, useEffect, useState } from 'react';
 import './App.css';
+import { IProduct } from './models/IProduct';
+import Product from './components/Product/Product';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+    const [products, setProducts] = useState<IProduct[]>();
+
+    useEffect(() => {
+        fetch('/api/v1/product/')
+            .then(response => response.json())
+            .then(data => setProducts(data));
+    }, []);
+
+    const product = products?.map(p =>
+                                      <Product key={ p.id } title={ p.title } price={ p.price }/>);
+
+    return <>
+        { product }
+    </>;
+};
 
 export default App;
