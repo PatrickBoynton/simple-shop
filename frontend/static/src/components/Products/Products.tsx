291bed969243e350c723ea5623e5e5549cee2c9d
PatrickBoynton <jpboynton3@gmail.com>
2021-09-01 16:51:34 -0400

    Set up the initial product display.

--- frontend/static/src/components/Products/Products.tsx
+++ frontend/static/src/components/Products/Products.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import { IProduct } from '../../models/IProduct';
 
-const Products = () => {
-    return <div>Products!</div>;
+const Products: FC = () => {
+    const [products, setProducts] = useState<IProduct[]>([]);
+
+    const fetchProducts = async (): Promise<void> => {
+        const response = await fetch('/api/v1/product/');
+        const data = await response.json();
+        setProducts(data);
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const productDisplay = products.map(prod => <Product key={ prod.id }
+                                                         title={ prod.title }
+                                                         price={ prod.price }/>);
+    return <>{ productDisplay }</>;
 };
 
 export default Products;
