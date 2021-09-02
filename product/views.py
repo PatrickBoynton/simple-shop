c0a6e5b6bff9af41d93a34a62483de8e69c20d24
PatrickBoynton <jpboynton3@gmail.com>
2021-09-02 15:22:17 -0400

    Added a new view for creating products.

--- product/views.py
+++ product/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
@@ -7,4 +6,8 @@ from .serializers import ProductSerializer
 # Create your views here.
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductCreateView(generics.CreateAPIView):
     serializer_class = ProductSerializer
