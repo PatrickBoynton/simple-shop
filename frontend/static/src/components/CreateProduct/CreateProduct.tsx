c676123fbad1d532cd1d4785b9be8b96f1358878
PatrickBoynton <jpboynton3@gmail.com>
2021-09-02 14:29:57 -0400

    Added a simple form to start with.

--- frontend/static/src/components/CreateProduct/CreateProduct.tsx
+++ frontend/static/src/components/CreateProduct/CreateProduct.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import './CreateProduct.css';
 
 const CreateProduct = () => {
-    return <>Create Product!</>;
+    return <form>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title"/>
+        <label htmlFor="price">Price</label>
+        <input type="number" name="price"/>
+        <button type="submit">Add Product</button>
+    </form>;
 };
 
 export default CreateProduct;
