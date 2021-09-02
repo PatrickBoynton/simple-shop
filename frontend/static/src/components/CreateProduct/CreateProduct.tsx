ccee68393dd1c00fd26c59c90b8c506aa5cb2dce
PatrickBoynton <jpboynton3@gmail.com>
2021-09-02 15:21:49 -0400

    Added a text area.

--- frontend/static/src/components/CreateProduct/CreateProduct.tsx
+++ frontend/static/src/components/CreateProduct/CreateProduct.tsx
@@ -7,6 +7,9 @@ const CreateProduct: FC = () => {
         <input type="text" name="title"/>
         <label htmlFor="price">Price</label>
         <input type="number" name="price"/>
+        <label htmlFor="description">Description</label>
+        <textarea name="description" cols={20} rows={3}>
+        </textarea>
         <button type="submit">Add Product</button>
     </form>;
 };
