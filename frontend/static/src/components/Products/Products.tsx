eda5547348accc9487aa9355bf09374664e65dff
PatrickBoynton <jpboynton3@gmail.com>
2021-09-02 14:58:48 -0400

    Used the custom hook.

--- frontend/static/src/components/Products/Products.tsx
+++ frontend/static/src/components/Products/Products.tsx
@@ -1,19 +1,9 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import Product from '../Product/Product';
-import { IProduct } from '../../models/IProduct';
+import { useFetchProducts } from '../../hooks/useProduct';
 
 const Products: FC = () => {
-    const [products, setProducts] = useState<IProduct[]>([]);
-
-    const fetchProducts = async (): Promise<void> => {
-        const response = await fetch('/api/v1/product/');
-        const data = await response.json();
-        setProducts(data);
-    };
-
-    useEffect(() => {
-        fetchProducts();
-    }, []);
+    const products = useFetchProducts();
 
     const productDisplay = products.map(prod => <Product key={ prod.id }
                                                          title={ prod.title }
