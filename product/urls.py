9bcde3137f035e30617cd4aff04586af15c7139a
PatrickBoynton <jpboynton3@gmail.com>
2021-09-02 15:22:06 -0400

    Added a path for product creation.

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import ProductListView
+from .views import ProductListView, ProductCreateView
 
 urlpatterns = [
-    path('', ProductListView.as_view())
+    path('', ProductListView.as_view()),
+    path('create/', ProductCreateView.as_view()),
 ]
