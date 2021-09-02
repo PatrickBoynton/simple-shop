925f4a52ba992d436fdaf26d493799a787036d5d
PatrickBoynton <jpboynton3@gmail.com>
2021-09-02 19:02:04 -0400

    Added logic for posting products.

--- frontend/static/src/components/CreateProduct/CreateProduct.tsx
+++ frontend/static/src/components/CreateProduct/CreateProduct.tsx
@@ -1,14 +1,38 @@
-import React, { FC } from 'react';
+import React, { FC, FormEvent, useState } from 'react';
 import './CreateProduct.css';
+import { useInput } from '../../hooks/useInput';
 
 const CreateProduct: FC = () => {
-    return <form>
+    const title = useInput('');
+    const price = useInput(0);
+    const description = useInput('');
+
+    const [product, setProduct] = useState({});
+
+    const handleSubmit = (event: FormEvent): void => {
+        event.preventDefault();
+        setProduct({title: title.value.toString(), price: +price.value, description: description.value.toString()});
+        fetchPostProduct();
+    };
+
+    const fetchPostProduct = async () => {
+        await fetch('api/v1/product/create/', {
+            method:  'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        });
+    };
+
+    return <form onSubmit={ handleSubmit }>
         <label htmlFor="title">Title</label>
-        <input type="text" name="title"/>
+        <input type="text" name="title" onChange={ title.onChange } value={ title.value }/>
         <label htmlFor="price">Price</label>
-        <input type="number" name="price"/>
+        <input type="number" name="price" onChange={ price.onChange } value={ price.value }/>
         <label htmlFor="description">Description</label>
-        <textarea name="description" cols={20} rows={3}>
+        <textarea name="description" cols={ 20 } rows={ 3 } onChange={ description.onChange }
+                  value={ description.value }>
         </textarea>
         <button type="submit">Add Product</button>
     </form>;
