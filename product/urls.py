401d4e1d9799f18d931d09654def58a43362e4be
PatrickBoynton <jpboynton3@gmail.com>
2021-09-07 19:09:00 -0400

    Added a url for deleting a product.

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
-from .views import ProductListView, ProductCreateView
+from .views import ProductListView, \
+                   ProductCreateView, \
+                   ProductDeleteView
 
 urlpatterns = [
     path('', ProductListView.as_view()),
     path('create/', ProductCreateView.as_view()),
+    path('<int:pk>/delete/', ProductDeleteView.as_view())
 ]
