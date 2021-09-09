1f71ddf2fa38da5e66fe520eec7b45f8802030b4
PatrickBoynton <jpboynton3@gmail.com>
2021-09-09 11:59:07 -0400

    Updated the views to reflect the new serializers. Added a detail and update view.

--- product/views.py
+++ product/views.py
@@ -1,19 +1,29 @@
 from rest_framework import generics
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductDetailSerializer, ProductListSerializer
 
 
 # Create your views here.
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
+
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductDetailSerializer
 
 
 class ProductCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
 
 
 class ProductDeleteView(generics.RetrieveDestroyAPIView):
     queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
+
+
+class ProductUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductListSerializer
