9126a8764acce231661bee051a01a5dfd723618b
PatrickBoynton <jpboynton3@gmail.com>
2021-09-08 22:08:33 -0400

    Added a faux admin check for testing purposes.

--- frontend/static/src/components/Product/Product.tsx
+++ frontend/static/src/components/Product/Product.tsx
@@ -2,11 +2,15 @@ import React, { FC } from 'react';
 import { IProduct } from '../../models/IProduct';
 import './Product.css';
 
-const Product: FC<IProduct> = ({title, price}: IProduct) => {
+const Product: FC<IProduct> = ({title, price, id, onDelete}: IProduct) => {
+    // For testing purposes.
+    const admin = true;
+
     return <ul className="container">
         <li className="card">
             <h1>{ title }</h1>
             <p>${ price }</p>
+            {admin ? <button onClick={() => onDelete(id)}>Delete Product</button> : null}
         </li>
     </ul>;
 };
