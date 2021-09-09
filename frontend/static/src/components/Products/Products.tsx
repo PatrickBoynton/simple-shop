d6afff731df0ca016935c015e24deb5e221424eb
PatrickBoynton <jpboynton3@gmail.com>
2021-09-08 22:09:46 -0400

    Added a delete method.

--- frontend/static/src/components/Products/Products.tsx
+++ frontend/static/src/components/Products/Products.tsx
@@ -1,11 +1,19 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import Product from '../Product/Product';
 import { useFetchProducts } from '../../hooks/useProduct';
 
 const Products: FC = () => {
     const products = useFetchProducts();
 
+    const deleteProducts = async (id: number): Promise<void> => {
+        await fetch(`api/v1/product/${ id }/delete`, {
+            method: 'DELETE'
+        });
+    };
+
     const productDisplay = products.map(prod => <Product key={ prod.id }
+                                                         id={ prod.id }
+                                                         onDelete={ deleteProducts }
                                                          title={ prod.title }
                                                          price={ prod.price }/>);
     return <>{ productDisplay }</>;
