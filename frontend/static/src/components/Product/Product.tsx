eefa13f7756eb75de35d9a3484fd6589e89a7bb1
PatrickBoynton <jpboynton3@gmail.com>
2021-09-14 01:27:16 -0400

    Added a redirect for editing.

--- frontend/static/src/components/Product/Product.tsx
+++ frontend/static/src/components/Product/Product.tsx
@@ -1,16 +1,25 @@
 import React, { FC } from 'react';
 import { IProduct } from '../../models/IProduct';
 import './Product.css';
+import { useHistory } from 'react-router';
 
 const Product: FC<IProduct> = ({title, price, id, onDelete}: IProduct) => {
     // For testing purposes.
     const admin = true;
+    const history = useHistory();
+
+    const redirect = (event: any) => {
+        history.push('/cart');
+    };
 
     return <ul className="container">
         <li className="card">
             <h1>{ title }</h1>
             <p>${ price }</p>
-            {admin ? <button onClick={() => onDelete(id)}>Delete Product</button> : null}
+            { admin ? (<>
+                <button onClick={ (e) =>  redirect(e) }>Edit Product</button>
+                <button onClick={ () => onDelete(id) }>Delete Product</button>
+            </>) : null }
         </li>
     </ul>;
 };
