8cd2f22c8e304278d8488b98dcdc04f18a02aa59
PatrickBoynton <jpboynton3@gmail.com>
2021-09-14 13:32:34 -0400

    Temporarily moved everything back into the main products component.

--- frontend/static/src/components/Products/Products.tsx
+++ frontend/static/src/components/Products/Products.tsx
@@ -1,9 +1,11 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import Product from '../Product/Product';
-import { useFetchProducts } from '../../hooks/useProduct';
+import './Products.css';
+import { IProduct } from '../../models/IProduct';
 
 const Products: FC = () => {
-    const products = useFetchProducts();
+    // This is temporary
+    const [products, setProducts] = useState<IProduct[]>([]);
 
     const deleteProducts = async (id: number): Promise<void> => {
         await fetch(`api/v1/product/${ id }/delete`, {
@@ -11,12 +13,22 @@ const Products: FC = () => {
         });
     };
 
+    const fetchProducts = async (url: string): Promise<void> => {
+        const response = await fetch(url);
+        const data = await response.json();
+        setProducts(data);
+    };
+
+    useEffect(() => {
+        fetchProducts('/api/v1/product');
+    }, []);
+
     const productDisplay = products.map(prod => <Product key={ prod.id }
                                                          id={ prod.id }
                                                          onDelete={ deleteProducts }
                                                          title={ prod.title }
                                                          price={ prod.price }/>);
-    return <>{ productDisplay }</>;
+    return <div className="container">{ productDisplay }</div>;
 };
 
 export default Products;
