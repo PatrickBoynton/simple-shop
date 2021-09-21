92dab88894146bf58f27d02c1695fa7dd5cd825d
PatrickBoynton <jpboynton3@gmail.com>
2021-09-21 15:54:22 -0400

    Added an initial model for the Cart.

--- cart/models.py
+++ cart/models.py
@@ -3,4 +3,15 @@ from django.db import models
 
 # Create your models here.
 class Cart(models.Model):
-    pass
+    product = models.ForeignKey('product.Product',
+                                on_delete=models.CASCADE,
+                                blank=True,
+                                null=True)
+
+    total_amount = models.DecimalField(max_digits=4,
+                                       decimal_places=2,
+                                       blank=True,
+                                       null=True)
+
+    def __str__(self):
+        return self.product
